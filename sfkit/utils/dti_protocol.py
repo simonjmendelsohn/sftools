ca44f2afcc6a3ffaf4172115da6a7da778d5d866
Denis Loginov <denis@stratologics.com>
2024-12-11 16:47:32 -0500

    Implement update_parameters for Secure-DTI

--- sfkit/utils/dti_protocol.py
+++ sfkit/utils/dti_protocol.py
@@ -29,45 +29,45 @@ def run_dti_protocol(role: str, demo: bool = False) -> None:
 def update_parameters(role: str) -> None:
     print(f"\n\n Updating parameters in 'secure-gwas/par/test.par.{role}.txt'\n\n")
 
-    # doc_ref_dict = get_doc_ref_dict()
+    doc_ref_dict = get_doc_ref_dict()
 
-    # # shared parameters and advanced parameters
-    # pars = {**doc_ref_dict["parameters"], **doc_ref_dict["advanced_parameters"]}
+    # shared parameters and advanced parameters
+    pars = {**doc_ref_dict["parameters"], **doc_ref_dict["advanced_parameters"]}
 
-    # # individual parameters
+    # individual parameters
     # for i in range(1, len(doc_ref_dict["participants"])):
     #     pars[f"NUM_INDS_SP_{i}"] = doc_ref_dict["personal_parameters"][doc_ref_dict["participants"][i]]["NUM_INDS"]
 
     # pars["NUM_INDS"] = {"value": ""}
     # pars["NUM_INDS"]["value"] = str(int(pars["NUM_INDS_SP_1"]["value"]) + int(pars["NUM_INDS_SP_2"]["value"]))
 
-    # # num threads = num_cpus = $(nproc)
-    # num_cpus = str(multiprocessing.cpu_count())
-    # pars["NUM_THREADS"] = {"value": num_cpus}
-    # update_firestore(f"update_firestore::NUM_THREADS={num_cpus}")
-    # update_firestore(f"update_firestore::NUM_CPUS={num_cpus}")
+    # num threads = num_cpus = $(nproc)
+    num_cpus = str(multiprocessing.cpu_count())
+    pars["NUM_THREADS"] = {"value": num_cpus}
+    update_firestore(f"update_firestore::NUM_THREADS={num_cpus}")
+    update_firestore(f"update_firestore::NUM_CPUS={num_cpus}")
 
-    # # update pars with ipaddresses and ports
-    # for i in range(len(doc_ref_dict["participants"])):
-    #     ip = doc_ref_dict["personal_parameters"][doc_ref_dict["participants"][i]]["IP_ADDRESS"]["value"]
-    #     while ip == "":
-    #         print(f"IP address for {doc_ref_dict['participants'][i]} is empty. Waiting...")
-    #         time.sleep(5)
+    # update pars with ipaddresses and ports
+    for i in range(len(doc_ref_dict["participants"])):
+        ip = doc_ref_dict["personal_parameters"][doc_ref_dict["participants"][i]]["IP_ADDRESS"]["value"]
+        while ip == "":
+            print(f"IP address for {doc_ref_dict['participants'][i]} is empty. Waiting...")
+            time.sleep(5)
 
-    #         doc_ref_dict = get_doc_ref_dict()
-    #         ip = doc_ref_dict["personal_parameters"][doc_ref_dict["participants"][i]]["IP_ADDRESS"]["value"]
+            doc_ref_dict = get_doc_ref_dict()
+            ip = doc_ref_dict["personal_parameters"][doc_ref_dict["participants"][i]]["IP_ADDRESS"]["value"]
 
-    #     pars[f"IP_ADDR_P{i}"] = {"value": ip}
+        pars[f"IP_ADDR_P{i}"] = {"value": ip}
 
-    #     ports = doc_ref_dict["personal_parameters"][doc_ref_dict["participants"][i]]["PORTS"]["value"]
-    #     for j in range(i + 1, 3):
-    #         pars[f"PORT_P{i}_P{j}"] = {"value": ports.split(",")[j]}
+        ports = doc_ref_dict["personal_parameters"][doc_ref_dict["participants"][i]]["PORTS"]["value"]
+        for j in range(i + 1, 3):
+            pars[f"PORT_P{i}_P{j}"] = {"value": ports.split(",")[j]}
 
-    # for line in fileinput.input(f"{constants.EXECUTABLES_PREFIX}secure-gwas/par/test.par.{role}.txt", inplace=True):
-    #     key = str(line).split(" ")[0]
-    #     if key in pars:
-    #         line = f"{key} " + str(pars[key]["value"]) + "\n"
-    #     print(line, end="")
+    for line in fileinput.input(f"{constants.EXECUTABLES_PREFIX}secure-dti/mpc/par/test.par.{role}.txt", inplace=True):
+        key = str(line).split(" ")[0]
+        if key in pars:
+            line = f"{key} " + str(pars[key]["value"]) + "\n"
+        print(line, end="")
 
 
 # def prepare_data(data_path: str, role: str) -> None:
