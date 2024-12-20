167c8d1e2ad16f7f8a664fa74f78854f566dc6b7
Denis Loginov <denis@stratologics.com>
2024-12-20 17:20:21 -0500

    Skip auto-creation of CP0 Secure-DTI for now

--- sfkit/protocol/run_protocol.py
+++ sfkit/protocol/run_protocol.py
@@ -54,7 +54,7 @@ def run_protocol(
             doc_ref_dict = get_doc_ref_dict()
             statuses = doc_ref_dict["status"]
 
-        if not demo and role == "1" and not skip_cp0 and study_type != "SF-RELATE":
+        if not demo and role == "1" and not skip_cp0 and study_type not in ["SF-RELATE", "Secure-DTI"]:
             create_cp0()
 
         if phase:
